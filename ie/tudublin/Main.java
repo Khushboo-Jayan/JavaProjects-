5a1f9d203790ffa36311e0c357628674de114da3
Khushboo Jayan <khushboojayan@gmail.com>
2022-02-15 02:45:58 +0000

    variables

--- ie/tudublin/Main.java
+++ ie/tudublin/Main.java
@@ -3,6 +3,18 @@ package ie.tudublin;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");        
+        System.out.println("Hello World!!"); 
+        
+        //variables 
+        byte age = 20;
+        int youtubeCount = 2_134_567_089; //_seperates every third digit like ,
+        long twitterLikes = 9_000_000_000L; //if more than 2 billion
+        float pi = 3.1415f;
+        boolean votingEligible = true;
+        System.out.println(age);
+        System.out.println(youtubeCount);
+        System.out.println(twitterLikes);
+        System.out.println(pi);
+        System.out.println(votingEligible);
     }
 }
