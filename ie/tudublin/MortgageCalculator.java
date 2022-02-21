ccffcc1ecabf6cbae2cfffc0c1d3a5cab0d3eb56
Khushboo Jayan <khushboojayan@gmail.com>
2022-02-21 00:16:39 +0000

    Addition of mortgage calculator

--- ie/tudublin/MortgageCalculator.java
+++ ie/tudublin/MortgageCalculator.java
@@ -9,29 +9,29 @@ public class MortgageCalculator {
         final byte PERCENT = 100;       //final is constant
         final byte MONTHS_YEAR = 12;
 
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Principle: ");
+            int principlevalue = scanner.nextInt();
 
-        System.out.print("Principle: ");
-        int principlevalue = scanner.nextInt();
+            System.out.print("Annual Interest Rate: ");       
+            float annualInterest = scanner.nextFloat();
+            float monthlyInterest = annualInterest / PERCENT /MONTHS_YEAR;
+             
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();    
+            int numberOfPayments = years * MONTHS_YEAR; //totalPayments is number of payments interest every month 
 
-        System.out.print("Annual Interest Rate: ");       
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT /MONTHS_YEAR;
-         
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();    
-        int numberOfPayments = years * MONTHS_YEAR; //totalPayments is number of payments interest every month 
+            // double mortgage = (principlevalue)*((actualInterest*Math.pow((1+actualInterest), months))/(Math.pow((1+actualInterest), months)) - 1);
+            double mortgage = principlevalue 
+                                * ((monthlyInterest * Math.pow(1+monthlyInterest, numberOfPayments ))
+                                / (Math.pow(1+monthlyInterest, numberOfPayments) - 1 ));
 
-        // double mortgage = (principlevalue)*((actualInterest*Math.pow((1+actualInterest), months))/(Math.pow((1+actualInterest), months)) - 1);
-        double mortgage = principlevalue 
-                            * ((monthlyInterest * Math.pow(1+monthlyInterest, numberOfPayments ))
-                            / (Math.pow(1+monthlyInterest, numberOfPayments) - 1 ));
-
-        String mortgageFormatted  = NumberFormat.getCurrencyInstance(Locale.CANADA).format(mortgage);
-        System.out.println(principlevalue);
-        System.out.println(monthlyInterest);
-        System.out.println(numberOfPayments);
-        System.out.println(mortgageFormatted);  
+            String mortgageFormatted  = NumberFormat.getCurrencyInstance(Locale.CANADA).format(mortgage);
+            System.out.println(principlevalue);
+            System.out.println(monthlyInterest);
+            System.out.println(numberOfPayments);
+            System.out.println(mortgageFormatted);
+        }  
 
 
     }
